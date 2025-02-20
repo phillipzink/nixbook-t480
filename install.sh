a85d003161cbdf168149a8798d6d41499d2e3bd7
phillipzink <40325165+phillipzink@users.noreply.github.com>
2025-02-20 15:13:11 -0500

    add nix-hardware channel

--- install.sh
+++ install.sh
@@ -5,8 +5,13 @@ read -p "Do you want to continue? (y/n): " answer
 
 if [[ "$answer" =~ ^[Yy]$ ]]; then
   echo "Installing T408/T480s configuration..."
-  # Add Nixbook config and rebuild
-  sudo sed -i '/hardware-configuration\.nix/a\      /etc/nixbook-t480/t480-configuration.nix' /etc/nixos/configuration.nix
+  # Add Nixbook config
+  sed -i '/hardware-configuration\.nix/a\      /etc/nixbook-t480/t480-configuration\.nix' /etc/nixos/configuration.nix
+
+  # Add nix-hardware channel and rebuild
+  nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
+  nix-channel --update
+  nixos-rebuild switch
 
   nix-shell -p powertop
   powertop --auto-tune
